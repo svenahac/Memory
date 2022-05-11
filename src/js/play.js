f78a1b0d00ac4a1ed6a969cf7d8526fbc2fc36cf
vexane <ahacsh@gmail.com>
2022-05-11 17:46:38 +0200

    finished

---
+++ src/js/play.js
@@ -0,0 +1,105 @@
+const pieces = document.querySelectorAll(".disable-select");
+let guessed = 0;
+let counter = 0;
+let minutes = 0;
+let counterInterval = undefined;
+
+function randomize() {
+  for (let i = 0; i < 500; i++) {
+    let randomnum1 = Math.floor(Math.random() * 15) + 1;
+    let randomnum2 = Math.floor(Math.random() * 15) + 1;
+    let sq1 = document.getElementById("piece" + randomnum1);
+    let sq2 = document.getElementById("piece" + randomnum2);
+
+    let sourcesq1 = sq1.parentNode.id;
+    let sourcesq2 = sq2.parentNode.id;
+
+    document.getElementById(sourcesq1).appendChild(sq2);
+    document.getElementById(sourcesq2).appendChild(sq1);
+  }
+}
+
+function flip(num) {
+  let el = document.getElementById("piece" + num);
+  el.classList.add("clicked");
+  let current = 0;
+  for (let j = 0; j < pieces.length; j++) {
+    if (pieces[j].classList.contains("clicked")) {
+      current++;
+    }
+  }
+  let num1 = el.innerHTML;
+  let num2 = 0;
+  let tempnum = 0;
+
+  for (let i = 0; i < pieces.length; i++) {
+    if (
+      pieces[i].classList.contains("clicked") &&
+      current == 2 &&
+      pieces[i] !== el
+    ) {
+      num2 = pieces[i].innerHTML;
+      tempnum = i;
+    }
+  }
+
+  if (current === 2 && num1 == num2) {
+    pieces[tempnum].classList.remove("clicked");
+    pieces[tempnum].classList.add("found");
+    el.classList.remove("clicked");
+    el.classList.add("found");
+    guessed++;
+  } else if (current === 2) {
+    window.setTimeout(function () {
+      for (let i = 0; i < pieces.length; i++) {
+        if (!pieces[i].classList.contains("found")) {
+          pieces[i].classList.remove("clicked");
+        }
+      }
+    }, 1000);
+    current = 0;
+  }
+  if (guessed === 8) {
+    window.alert("Congrats your time was: " + minutes + ":" + counter);
+    guessed = 0;
+    randomize();
+    stopCounter();
+    document.location.reload();
+    for (let i = 0; i < pieces.length; i++) {
+      pieces[i].classList.remove("found");
+    }
+  }
+}
+
+function increaseCounter() {
+  counter++;
+  let number = document.getElementById("sec");
+  let mins = document.getElementById("min");
+  number.innerHTML = counter;
+  if (counter > 9) {
+    number.classList.add("sec-double");
+  }
+  if (counter === 60) {
+    counter = 0;
+    minutes++;
+    number.innerHTML = 0;
+    mins.innerHTML = minutes;
+  }
+}
+
+function startCounter() {
+  if (counterInterval === undefined) {
+    counterInterval = window.setInterval(increaseCounter, 1000);
+  }
+}
+
+function stopCounter() {
+  window.clearInterval(counterInterval);
+  counterInterval = undefined;
+  counter = 0;
+  minutes = 0;
+  document.getElementById("sec").innerHTML = 0;
+  document.getElementById("min").innerHTML = 0;
+}
+
+randomize();
